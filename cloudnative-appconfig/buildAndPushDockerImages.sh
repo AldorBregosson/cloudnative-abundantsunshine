bfce875370164132476a85b4257b2acac888c445
AldorBregosson <Aldor.Bregosson@gmail.com>
2021-05-28 16:01:11 +0200

    adapted github user

--- cloudnative-appconfig/buildAndPushDockerImages.sh
+++ cloudnative-appconfig/buildAndPushDockerImages.sh
@@ -1,15 +1,16 @@
+#!/usr/local/bin/bash
 docker build --build-arg \
 jar_file=cloudnative-connectionposts/target/cloudnative-connectionposts-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-appconfig-connectionposts:0.0.1 .
+-t aldorbregosson/cloudnative-appconfig-connectionposts:0.0.1 .
 #
 docker build --build-arg \
 jar_file=cloudnative-connections/target/cloudnative-connections-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-appconfig-connections:0.0.1 .
+-t aldorbregosson/cloudnative-appconfig-connections:0.0.1 .
 #
 docker build --build-arg \
 jar_file=cloudnative-posts/target/cloudnative-posts-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-appconfig-posts:0.0.1 .
+-t aldorbregosson/cloudnative-appconfig-posts:0.0.1 .
 #
-docker push cdavisafc/cloudnative-appconfig-connectionposts:0.0.1
-docker push cdavisafc/cloudnative-appconfig-connections:0.0.1
-docker push cdavisafc/cloudnative-appconfig-posts:0.0.1
+docker push aldorbregosson/cloudnative-appconfig-connectionposts:0.0.1
+docker push aldorbregosson/cloudnative-appconfig-connections:0.0.1
+docker push aldorbregosson/cloudnative-appconfig-posts:0.0.1
