d3a11de3b30349302ccb7504ff7074140fd8a6e5
AldorBregosson <Aldor.Bregosson@gmail.com>
2021-05-28 16:12:02 +0200

    adapted github user

--- cloudnative-applifecycle/buildAndPushDockerImages.sh
+++ cloudnative-applifecycle/buildAndPushDockerImages.sh
@@ -1,15 +1,16 @@
+#!/usr/local/bin/bash
 docker build --build-arg \
 jar_file=cloudnative-connectionposts/target/cloudnative-connectionposts-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-applifecycle-connectionposts:0.0.2 .
+-t aldorbregosson/cloudnative-applifecycle-connectionposts:0.0.2 .
 #
 docker build --build-arg \
 jar_file=cloudnative-connections/target/cloudnative-connections-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-applifecycle-connections:0.0.2 .
+-t aldorbregosson/cloudnative-applifecycle-connections:0.0.2 .
 #
 docker build --build-arg \
 jar_file=cloudnative-posts/target/cloudnative-posts-0.0.1-SNAPSHOT.jar \
--t cdavisafc/cloudnative-applifecycle-posts0.0.2 .
+-t aldorbregosson/cloudnative-applifecycle-posts0.0.2 .
 #
-docker push cdavisafc/cloudnative-applifecycle-connectionposts:0.0.2
-docker push cdavisafc/cloudnative-applifecycle-connections:0.0.2
-docker push cdavisafc/cloudnative-applifecycle-posts:0.0.2
+docker push aldorbregosson/cloudnative-applifecycle-connectionposts:0.0.2
+docker push aldorbregosson/cloudnative-applifecycle-connections:0.0.2
+docker push aldorbregosson/cloudnative-applifecycle-posts:0.0.2
