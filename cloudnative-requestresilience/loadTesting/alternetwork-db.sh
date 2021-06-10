62b2a4a32d064cd35b65b48faebe9c5c1d9c85ba
AldorBregosson <Aldor.Bregosson@gmail.com>
2021-06-10 17:23:27 +0200

    adapted the network disruption simulation script

--- cloudnative-requestresilience/loadTesting/alternetwork-db.sh
+++ cloudnative-requestresilience/loadTesting/alternetwork-db.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 if [ $1 == "add" ]; then
     action="breaking"
+    kubectl exec mysql-698d9857bc-rk9l8 -- route $1 -host 172.17.0.13 reject
+    kubectl exec mysql-698d9857bc-rk9l8 -- route $1 -host 172.17.0.14 reject
+    kubectl exec mysql-698d9857bc-rk9l8 -- route $1 -host 172.17.0.15 reject
+    kubectl exec mysql-698d9857bc-rk9l8 -- route $1 -host 172.17.0.16 reject
 else
     action="fixing"
+    # we may want to add commands for fixing the network connections again:
+    kubectl exec mysql-698d9857bc-rk9l8 -- route delete -host 172.17.0.16 reject
+    kubectl exec mysql-698d9857bc-rk9l8 -- route delete -host 172.17.0.15 reject
+    kubectl exec mysql-698d9857bc-rk9l8 -- route delete -host 172.17.0.14 reject
+    kubectl exec mysql-698d9857bc-rk9l8 -- route delete -host 172.17.0.13 reject
 fi
 echo "$action 5 connections"
-kubectl exec mysql-57bdb878f5-dhlck -- route $1 -host 10.36.4.11 reject
-kubectl exec mysql-57bdb878f5-dhlck -- route $1 -host 10.36.3.11 reject
-kubectl exec mysql-57bdb878f5-dhlck -- route $1 -host 10.36.5.9 reject
-kubectl exec mysql-57bdb878f5-dhlck -- route $1 -host 10.36.1.13 reject
