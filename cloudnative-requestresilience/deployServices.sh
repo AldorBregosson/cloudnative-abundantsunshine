2ef519d95738dfebe2fea299c1850fdc2f3e54fd
AldorBregosson <Aldor.Bregosson@gmail.com>
2021-06-10 10:12:03 +0200

    cleaned up shell scripts

--- cloudnative-requestresilience/deployServices.sh
+++ cloudnative-requestresilience/deployServices.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 kubectl apply -f spring-cloud-config-server-deployment-kubernetes.yaml
 kubectl apply -f redis-deployment.yaml
 kubectl apply -f mysql-deployment.yaml
 # after creating mysql must create the cookbook database
-
