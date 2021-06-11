d53e101dc27a076bc111cd4d8c3e93d7d4fefd76
AldorBregosson <Aldor.Bregosson@gmail.com>
2021-06-11 10:10:32 +0200

    fixed network disruption script

--- cloudnative-requestresilience/loadTesting/alternetwork-db.sh
+++ cloudnative-requestresilience/loadTesting/alternetwork-db.sh
@@ -7,7 +7,6 @@ if [ -z "$1" ]; then
 fi
 if [ $1 == "add" ]; then
     action="breaking"
-
 fi
 if [ $1 == "delete" ]; then
     action="fixing"
@@ -18,3 +17,4 @@ kubectl exec mysql-698d9857bc-rk9l8 -- route $1 -host 172.17.0.15 reject
 kubectl exec mysql-698d9857bc-rk9l8 -- route $1 -host 172.17.0.16 reject
 # actually, it's FOUR connections from posts instances to the database.
 echo "$action 4 connections"
+
