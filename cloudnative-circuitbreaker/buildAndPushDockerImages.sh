8ce1dcc765ab5f4ca8418c0cb21fdb7b097bcff0
AldorBregosson <Aldor.Bregosson@gmail.com>
2021-06-15 16:32:37 +0200

    adapted docker build script

--- cloudnative-circuitbreaker/buildAndPushDockerImages.sh
+++ cloudnative-circuitbreaker/buildAndPushDockerImages.sh
@@ -3,3 +3,4 @@ jar_file=cloudnative-posts/target/cloudnative-posts-0.0.1-SNAPSHOT.jar \
 -t aldorbregosson/cloudnative-circuitbreaker-posts:0.0.1 .
 #
 docker push aldorbregosson/cloudnative-circuitbreaker-posts:0.0.1
+
